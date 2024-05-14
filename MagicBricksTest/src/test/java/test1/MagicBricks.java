1c64f89a63d38c956e5212510514d81b6bb0ebc7
nileshladwanshi <nileshladwanshi@gmail.com>
2024-05-14 11:11:54 +0530

    Updated some xpath

--- MagicBricksTest/src/test/java/test1/MagicBricks.java
+++ MagicBricksTest/src/test/java/test1/MagicBricks.java
@@ -25,42 +25,45 @@ public class MagicBricks {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
+		//driver.findElement(By.className("mb-search__tag-close")).click(); // optional if by default getting any city name
 		// Input location Mumbai and click submit
 		driver.findElement(By.className("mb-search__input")).sendKeys("Mumbai");
+		//driver.findElement(By.xpath("//div[@id=\"serachSuggest\"]/div[3]")).click();  // optional if by default getting any city
 		driver.findElement(By.className("mb-search__btn")).click();
+		
+		//sorting in decending order
+		driver.findElement(By.className("mb-srp__tabs__sortby--title")).click();
+		driver.findElement(By.xpath("//ul[@class='mb-srp__tabs__sortby__dd__list']/li[text()='Price - High to Low']")).click();
 		Thread.sleep(1000);
 		List<WebElement> prices = driver.findElements(By.xpath("//div[@class='mb-srp__card__price--amount']"));
-		TreeSet<WebElement> priceSet = new TreeSet<>(prices);
 		int count = 0;
 
 		// Print all price values in sorted order(descending)
-		for (WebElement price : priceSet.descendingSet()) {
+		for (WebElement price : prices) 
+		{
 			System.out.println(price.getText());
 			count++;
 			if (count == 5) {
 				price.click();// Click on the 5th element
 			}
 		}
-		String parent = driver.getWindowHandle();
 		Set<String> s = driver.getWindowHandles();
 		Iterator<String> I1 = s.iterator();
-		while (I1.hasNext()) {
-			String child_window = I1.next();
-			if (!parent.equals(child_window)) {
-				driver.switchTo().window(child_window);
-
-				// Click on Get Phone No. button
-				driver.findElement(By.className("contact-form__btn freecab")).click();
-				driver.findElement(By.id("userName")).sendKeys("Nilesh");
-				driver.findElement(By.id("userEmail")).sendKeys("xyz@gmail.com");
-				driver.findElement(By.id("userMobile")).sendKeys("9952752254");
-				driver.findElement(By.className("contact-form__btn")).click();
-				// Fill the form and submited
-				driver.quit();
-				// Close the browser
+		String parent_window = I1.next();
+		String child_window = I1.next();
+		Thread.sleep(1000);
+		driver.switchTo().window(child_window);
+		driver.findElement(By.xpath("//button[@class='contact-form__btn freecab']")).click();
+		driver.findElement(By.id("userName")).sendKeys("Nilesh");
+		driver.findElement(By.id("userEmail")).sendKeys("xyz@gmail.com");
+		driver.findElement(By.id("userMobile")).sendKeys("9952752254");
+		driver.findElement(By.className("contact-form__btn")).click();
+		// Fill the form and submited
+		driver.quit();
+		// Close the browser
 			}
-		}
+		
 
-	}
+	
 
 }
